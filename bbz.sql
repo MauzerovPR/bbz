8b6e54d81696dc99ec36d1db4b4e946f2f4f6ee5
Mauzerov <mauzer.official.1@gmail.com>
2023-10-31 20:17:28 +0100

    add a couple of not null

--- bbz.sql
+++ bbz.sql
@@ -12,14 +12,14 @@ CREATE TABLE IF NOT EXISTS Samochody (
     rodzaj_paliwa varchar(255),
     przebieg bigint,
     numer_rej varchar(20),
-    cena decimal(10, 2)
+    cena decimal(10, 2) not null
 );
 
 CREATE TABLE IF NOT EXISTS Komitenci (
 	komitenci_id int primary key auto_increment,
     imie varchar(255),
     nazwisko varchar(255),
-    pesel varchar(11) check ( length(pesel) = 11 ),
+    pesel varchar(11) check ( length(pesel) = 11 ) not null ,
     e_mail varchar(255) default null,
     telefon varchar(20) default null,
     adres text default null,
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS Nabywcy (
 	nabywcy_id int primary key auto_increment,
     imie varchar(255),
     nazwisko varchar(255),
-    pesel varchar(11) check ( length(pesel) = 11 ),
+    pesel varchar(11) check ( length(pesel) = 11 ) not null,
     e_mail varchar(255) default null,
     telefon varchar(20) default null,
     adres text default null,
@@ -42,8 +42,8 @@ CREATE TABLE IF NOT EXISTS Rejestr (
     komitenci_id int not null,
     nabywcy_id int not null,
     samochody_id int not null,
-    data_zakup timestamp default current_timestamp(),
-    cena decimal(10, 2),
+    data_zakup timestamp default current_timestamp() not null,
+    cena decimal(10, 2) not null,
     foreign key (komitenci_id) references Komitenci(komitenci_id),
     foreign key (nabywcy_id) references Nabywcy(nabywcy_id),
     foreign key (samochody_id) references Samochody(samochody_id)
